489bddbcf4fe637b2b483417d7074f4b8e4fb36c
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2019-11-19 22:52:09 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import 
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function About() {
+  return <h1>My name is Mat</h1>
 }
 
-export default App;
+function Contact() {
+  return <h1>You can finde me here :</h1>
+}
+
+function Home() {
+  return <h1>Home</h1>
+}
+
+function NotFound() {
+  return <h1>404 - not found</h1>
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink activeClassName="nav-active" to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName="nav-active" to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName="nav-active" to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+          </nav>
+  
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
+}
+
+export default App
