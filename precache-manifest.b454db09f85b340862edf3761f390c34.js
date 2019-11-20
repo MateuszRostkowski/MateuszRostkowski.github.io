2c08abf44026c08f72f338a9ba89ff001ab1e0e9
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2019-11-20 20:37:33 +0100

    Updates

---
+++ precache-manifest.b454db09f85b340862edf3761f390c34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7861bb0f233c1071e2761157ce33343e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f275750c178803a4b0f6",
+    "url": "/static/css/main.c6125388.chunk.css"
+  },
+  {
+    "revision": "e37bdfeea9f4db7c331e",
+    "url": "/static/js/2.a00b43c0.chunk.js"
+  },
+  {
+    "revision": "f275750c178803a4b0f6",
+    "url": "/static/js/main.f3c88c7f.chunk.js"
+  },
+  {
+    "revision": "1678d8a5bec8bfd0e7d9",
+    "url": "/static/js/runtime-main.6cc56eda.js"
+  }
+]);
