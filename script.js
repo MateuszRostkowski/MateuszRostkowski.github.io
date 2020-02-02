d1d74054a7d25261f6ae9fb525588ec7a7e45ed0
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2020-02-02 19:12:16 +0100

    add setHightVariable that calculates stylesheet variable

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const menuBtn = document.querySelector("#menuBtn");
 const siteMenu = document.querySelector("#siteMenu");
 
 function toggleMenu() {
-  console.log("XD");
   menuBtn.classList.toggle("change");
   siteMenu.classList.toggle("shown");
 }
@@ -31,17 +30,13 @@ infoIcons.forEach(icon => {
   });
 });
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty("--vh", `${vh}px`);
+function setHeightVariable() {
+    let vh = window.innerHeight * 0.01 - 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+setHeightVariable()
 
-// We listen to the resize event
-window.addEventListener("resize", () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+window.addEventListener("resize", setHeightVariable);
 
 const data = [
   {
