97b5a0b29574215359210bd991f2b9cf0526c7cc
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2020-02-02 19:04:59 +0100

    Refactor code

--- script.js
+++ script.js
@@ -1,131 +1,134 @@
-const infoIcons = document.querySelectorAll(".info__icon")
-const moreInfoElements = document.querySelectorAll(".more-info")
-const closeButtons = document.querySelectorAll(".more-info__close")
+const infoIcons = document.querySelectorAll(".info__icon");
+const moreInfoElements = document.querySelectorAll(".more-info");
+const closeButtons = document.querySelectorAll(".more-info__close");
 
-const menuBtn = document.querySelector("#menuBtn")
-const siteMenu = document.querySelector("#siteMenu")
-
+const menuBtn = document.querySelector("#menuBtn");
+const siteMenu = document.querySelector("#siteMenu");
 
 function toggleMenu() {
-    console.log("XD")
-    menuBtn.classList.toggle("change");
-    siteMenu.classList.toggle("shown");
+  console.log("XD");
+  menuBtn.classList.toggle("change");
+  siteMenu.classList.toggle("shown");
 }
 
-menuBtn.addEventListener('click', toggleMenu)
+menuBtn.addEventListener("click", toggleMenu);
 
 function closeInfo() {
-    moreInfoElements.forEach((element) => {
-        element.classList.add('more-info--hidden')
-    })
+  moreInfoElements.forEach(element => {
+    element.classList.add("more-info--hidden");
+  });
 }
 
-closeButtons.forEach((button) => {
-    button.addEventListener('click', closeInfo)
-})
+closeButtons.forEach(button => {
+  button.addEventListener("click", closeInfo);
+});
 
-
-infoIcons.forEach((icon) => {
-    icon.addEventListener('click', () => {
-        const moreInfo = icon.parentElement.querySelector('.more-info')
-        closeInfo()
-        moreInfo.classList.remove('more-info--hidden')
-    })
-})
+infoIcons.forEach(icon => {
+  icon.addEventListener("click", () => {
+    const moreInfo = icon.parentElement.querySelector(".more-info");
+    closeInfo();
+    moreInfo.classList.remove("more-info--hidden");
+  });
+});
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 // We listen to the resize event
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener("resize", () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
 const data = [
-    {
-        name: "LiveChat",
-        shortDesc: "App",
-        liveDemo: "http://jsmessenger.surge.sh/",
-        githubLink: "https://github.com/MateuszRostkowski/RockPaperScissors",
-        moreInfo: "This is a simple RockPaperScissors game made in vanilla Javascript",
-        moreInfoPhoto: "Live chat made in vanilla javascript."
-    },
-    {
-        name: "RockPaperScissors",
-        shortDesc: "Game in JS",
-        liveDemo: "",
-        githubLink: "https://github.com/MateuszRostkowski/RockPaperScissors",
-        moreInfo: "This is a simple RockPaperScissors game made in vanilla Javascript",
-        moreInfoPhoto: ""
-    },
-    {
-        name: "Police and Thives",
-        shortDesc: "Game in JS",
-        liveDemo: "https://police-and-thives.netlify.com/",
-        githubLink: "https://github.com/MateuszRostkowski/PoliceAndThives",
-        moreInfo: "Simple Game made in vanilla javascript.",
-        moreInfoPhoto: ""
-    },
-    {
-        name: "Concerte",
-        shortDesc: "Team Project",
-        liveDemo: "http://www.frontczewscy.jfdd13.is-academy.pl/",
-        githubLink: "https://github.com/infoshareacademy/jfdd13-frontczewscy",
-        moreInfo: "Team Project that was made on a bootcamp. Made with HTML, CSS and Javascript.",
-        moreInfoPhoto: ""
-    },
-    {
-        name: "Concerte App",
-        shortDesc: "Team Project",
-        liveDemo: "http://app.frontczewscy.jfdd13.is-academy.pl/",
-        githubLink: "https://github.com/infoshareacademy/jfdd13-frontczewscy-app",
-        moreInfo: "Team Project that was made on a bootcamp. Made with ReactJS, firebase and more.",
-        moreInfoPhoto: ""
-    },
-    {
-        name: "TicTacToe",
-        shortDesc: "Simple Game in React",
-        liveDemo: "https://mateuszrostkowski.github.io/TicTacToe/",
-        githubLink: "https://github.com/MateuszRostkowski/TicTacToe",
-        moreInfo: "Simple TicTacToe game made in React",
-        moreInfoPhoto: ""
-    },
-    {
-        name: "JavascriptMasters",
-        shortDesc: "Gatsby blog about Javascript",
-        liveDemo: "http://javascriptmasters.netlify.com/",
-        githubLink: "https://github.com/MateuszRostkowski/Gatsby_blog",
-        moreInfo: "This blog was made using Gatsby",
-        moreInfoPhoto: ""
-    },
-    {
-        name: "Weather app",
-        shortDesc: "simple javascript app",
-        liveDemo: "https://weather-location.netlify.com/",
-        githubLink: "",
-        moreInfo: "Weather app made with vanilla javascript. You need to allow use your location to fully use this site.",
-        moreInfoPhoto: ""
-    },
-    {
-        name: "RockPaperScissors",
-        shortDesc: "Game in JS",
-        liveDemo: "http://rock-paper-sizor.surge.sh/",
-        githubLink: "https://github.com/MateuszRostkowski/RockPaperScissors",
-        moreInfo: "This is a simple RockPaperScissors game made in vanilla Javascript",
-        moreInfoPhoto: ""
-    },
-]
+  {
+    name: "LiveChat",
+    shortDesc: "App",
+    liveDemo: "http://jsmessenger.surge.sh/",
+    githubLink: "https://github.com/MateuszRostkowski/RockPaperScissors",
+    moreInfo:
+      "This is a simple RockPaperScissors game made in vanilla Javascript",
+    moreInfoPhoto: "Live chat made in vanilla javascript."
+  },
+  {
+    name: "RockPaperScissors",
+    shortDesc: "Game in JS",
+    liveDemo: "",
+    githubLink: "https://github.com/MateuszRostkowski/RockPaperScissors",
+    moreInfo:
+      "This is a simple RockPaperScissors game made in vanilla Javascript",
+    moreInfoPhoto: ""
+  },
+  {
+    name: "Police and Thives",
+    shortDesc: "Game in JS",
+    liveDemo: "https://police-and-thives.netlify.com/",
+    githubLink: "https://github.com/MateuszRostkowski/PoliceAndThives",
+    moreInfo: "Simple Game made in vanilla javascript.",
+    moreInfoPhoto: ""
+  },
+  {
+    name: "Concerte",
+    shortDesc: "Team Project",
+    liveDemo: "http://www.frontczewscy.jfdd13.is-academy.pl/",
+    githubLink: "https://github.com/infoshareacademy/jfdd13-frontczewscy",
+    moreInfo:
+      "Team Project that was made on a bootcamp. Made with HTML, CSS and Javascript.",
+    moreInfoPhoto: ""
+  },
+  {
+    name: "Concerte App",
+    shortDesc: "Team Project",
+    liveDemo: "http://app.frontczewscy.jfdd13.is-academy.pl/",
+    githubLink: "https://github.com/infoshareacademy/jfdd13-frontczewscy-app",
+    moreInfo:
+      "Team Project that was made on a bootcamp. Made with ReactJS, firebase and more.",
+    moreInfoPhoto: ""
+  },
+  {
+    name: "TicTacToe",
+    shortDesc: "Simple Game in React",
+    liveDemo: "https://mateuszrostkowski.github.io/TicTacToe/",
+    githubLink: "https://github.com/MateuszRostkowski/TicTacToe",
+    moreInfo: "Simple TicTacToe game made in React",
+    moreInfoPhoto: ""
+  },
+  {
+    name: "JavascriptMasters",
+    shortDesc: "Gatsby blog about Javascript",
+    liveDemo: "http://javascriptmasters.netlify.com/",
+    githubLink: "https://github.com/MateuszRostkowski/Gatsby_blog",
+    moreInfo: "This blog was made using Gatsby",
+    moreInfoPhoto: ""
+  },
+  {
+    name: "Weather app",
+    shortDesc: "simple javascript app",
+    liveDemo: "https://weather-location.netlify.com/",
+    githubLink: "",
+    moreInfo:
+      "Weather app made with vanilla javascript. You need to allow use your location to fully use this site.",
+    moreInfoPhoto: ""
+  },
+  {
+    name: "RockPaperScissors",
+    shortDesc: "Game in JS",
+    liveDemo: "http://rock-paper-sizor.surge.sh/",
+    githubLink: "https://github.com/MateuszRostkowski/RockPaperScissors",
+    moreInfo:
+      "This is a simple RockPaperScissors game made in vanilla Javascript",
+    moreInfoPhoto: ""
+  }
+];
 
-const jsContainer = document.querySelector("#jsContainer")
+const jsContainer = document.querySelector("#jsContainer");
 
-data.forEach((element) => {
-    const { name, shortDesc, githubLink, moreInfo, moreInfoPhoto } = element
-    jsContainer.innerHTML +=  `
+data.forEach(element => {
+  const { name, shortDesc, githubLink, moreInfo, moreInfoPhoto } = element;
+  jsContainer.innerHTML += `
         <div class="container__item">
             <a href=""><span class="app__name">${name}</span> - ${shortDesc}</a>
             <a href="${githubLink}">
@@ -137,5 +140,5 @@ data.forEach((element) => {
                 ${moreInfo}
             </div>
         </div> 
-    `
-})
+    `;
+});
